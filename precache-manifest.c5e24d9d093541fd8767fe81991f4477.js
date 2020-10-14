a7d0ca8d982c1ffa5d30afa7b5975288c1b493b2
zhangqingzi <lethanhtruongvinh99@gmail.com>
2020-10-14 21:17:41 +0700

    Updates

---
+++ precache-manifest.c5e24d9d093541fd8767fe81991f4477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679dd34b10edd4b03799e8b039ba52ce",
+    "url": "/completetictactoe/index.html"
+  },
+  {
+    "revision": "64631b73c6bff5e861e9",
+    "url": "/completetictactoe/static/css/main.03353089.chunk.css"
+  },
+  {
+    "revision": "0a882649cc4ec81cd9e5",
+    "url": "/completetictactoe/static/js/2.cd816c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/completetictactoe/static/js/2.cd816c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64631b73c6bff5e861e9",
+    "url": "/completetictactoe/static/js/main.bc38143f.chunk.js"
+  },
+  {
+    "revision": "b189e2e48f4e14e5e33f",
+    "url": "/completetictactoe/static/js/runtime-main.61e61df3.js"
+  }
+]);
